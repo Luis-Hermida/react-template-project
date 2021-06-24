c68cce54917b2db358e16c3ef55eb16656eb94c8
Luis Alejandro Hermida <42404237+Luis-Hermida@users.noreply.github.com>
2021-06-24 01:55:58 -0500

    Revert "Added custom theme support"
    This reverts commit ab58141e700ada2a563eae6418470a9eb8b8bb32.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,26 @@
-import React, { useContext } from "react";
-import "./App.css";
-import { Button } from "@material-ui/core";
-import { ThemeContext } from "./ThemeProvider";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App: React.FC = () => {
-  const setThemeName = useContext(ThemeContext);
-
+function App() {
   return (
     <div className="App">
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => setThemeName("lightTheme")}
-      >
-        Set Light Theme
-      </Button>
-      <Button
-        variant="contained"
-        color="secondary"
-        onClick={() => setThemeName("darkTheme")}
-      >
-        Set Dark Theme
-      </Button>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
